7f11af0a524a7e0175ca86bfb2d52c12cf369cda
Cristian Adam <Cristian@Aiur.Milkyway.Adam4.com>
2015-11-21 12:36:57 -0500

    interrupts and motion code prototype

--- Arduino/MotionCapture/MotionCapture.ino
+++ Arduino/MotionCapture/MotionCapture.ino
@@ -1,7 +1,9 @@
-//MPU 6050
-//short example
-
 #include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+
+
 
 //setup constant variable for MPU address
 const int MPU = 0x68;
@@ -11,6 +13,30 @@ int16_t AcX, AcY, AcZ;
 int16_t GyX, GyY, GyZ;
 //setup variable to read temperature
 int16_t Tmp;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage 
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// not sure if this is needed
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+}
 
 void setup()
 {
@@ -26,10 +52,36 @@ void setup()
   //Serial communication
   Serial.begin(9600);
   //Serial.println("It starts!");
+
+// Setup interrupt routine
+  cli();
+  // Set the registers
+  TCCR1A = 0; //Timer Counter Control register
+  // Set mode
+  TCCR1B = (1 << WGM12); // turn on CTC mode
+  // Set prescale values (1024).
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  //Enable timer compare interrupt===> TIMSK1
+  TIMSK1 |= (1 << OCIE1A);
+  // Set OCR1A
+  OCR1A = 624;
+  // Enable global interrupts
+  sei();
 }
 
-void loop()
+
+
+// use timer interrupt instead of loop for more constant time percision
+ISR (TIMER1_COMPA_vect) 
 {
+  // echo any serial data recieved to allow communication
+   if (Serial.available() > 0) 
+   {
+      incomingByte = Serial.read();
+      Serial.print("echo: ");
+      Serial.println(incomingByte, DEC);
+   }
+        
   Wire.beginTransmission(MPU);
   //start with register 0x3B ---- ACCEL_XOUT_H
   Wire.write(0x3B);
@@ -72,9 +124,8 @@ void loop()
   //Serial.println("");
   Serial.print(" | ");
   Serial.print("Tmp=");
-  //specail equation for tempuration calculation
+  //temperature calibration
   Serial.print(Tmp/340.00+36.53);
-  //Serial.println("");
   Serial.print(" | ");
   Serial.print("GyX=");
   Serial.print(GyX);
@@ -85,7 +136,7 @@ void loop()
   Serial.print("GyZ=");
   Serial.print(GyZ);
   Serial.println("");
-
-  //print delay
-  delay(300);
 }
+
+// needed to keep the IDE happy
+void loop(){}
